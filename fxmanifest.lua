14ecfbe5d3f9a431ea105fd0cac1fb25a77ed3e3
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-02-19 20:07:51 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'REDLINE CITY RP | RCRP'
+description 'Restricted zone Script'
+version 'v1.0 (release)'
+
+shared_scripts {
+    '@es_extended/imports.lua',
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_script 'client.lua'
+server_script 'server.lua'
