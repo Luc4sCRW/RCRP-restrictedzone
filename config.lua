a81720e6b4915359e9878806b2df40742ef44ecd
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-02-19 20:09:08 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,29 @@
+Config = {}
+
+Config.NotifyType = "custom" -- "bulletin", "esx", "gta", "ox", "custom" (add you own event/export at the server.lua)
+
+Config.AuthorizedJobs = {
+    ['police'] = true,
+    ['noose'] = true
+}
+
+Config.RadiusOptions = {
+    { label = '10m', value = 10.0 },
+    { label = '20m', value = 20.0 },
+    { label = '30m', value = 30.0 },
+    { label = '50m', value = 50.0 },
+    { label = '80m', value = 80.0 },
+    { label = '100m', value = 100.0 },
+    { label = '150m', value = 150.0 },
+    { label = '200m', value = 200.0 },
+    { label = '300m', value = 300.0 },
+}
+
+Config.TimeOptions = {
+    { label = '10 Sek (Debug Test)', value = 0.166 },
+    { label = '5 Min', value = 5 },
+    { label = '10 Min', value = 10 },
+    { label = '20 Min', value = 20 },
+    { label = '30 Min', value = 30 },
+    { label = '60 Min', value = 60 }
+}
