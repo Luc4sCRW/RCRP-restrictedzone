1177dd431f7558c2cbb0440e9fe291c6b47d0859
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-02-20 19:08:00 +0100

    Update fxmanifest.lua
    Added locales.lua and all locale files to shared scripts.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,8 +8,13 @@ version 'v1.0 (release)'
 shared_scripts {
     '@es_extended/imports.lua',
     '@ox_lib/init.lua',
-    'config.lua'
+    'config.lua',
+    
+    'locales.lua',
+    'locales/*.lua'
 }
 
 client_script 'client.lua'
 server_script 'server.lua'
+
+-- NEXT UPDATE: delete menu mit den einzelnen sperrzonen die errichtet sind mit straßen name weil alle löschen wenn da iwas ist is kacke
