a10c32dc7b6403cd9374ffc169d46ec83a8978fd
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-05-21 12:24:01 +0200

    Update de.lua

--- locales/de.lua
+++ locales/de.lua
@@ -1,23 +1,24 @@
 Locales['de'] = {
     ['access_denied'] = 'Zugriff verweigert',
-    ['menu_title'] = 'Sperrzonen Management',
+    ['menu_title'] = 'Sperrzonen Management', -- Fallback
     ['create_zone'] = 'Neue Sperrzone erstellen',
     ['create_zone_desc'] = 'Zone an aktueller Position erstellen',
-    ['delete_zones'] = 'Alle Sperrzonen löschen',
-    ['delete_zones_desc'] = 'Alle aktiven Zonen entfernen',
+    ['delete_spec_zones'] = 'Sperrzone löschen',
+    ['delete_spec_zones_desc'] = 'Zeigt eine Liste aller aktiven Zonen an',
+    ['delete_select'] = 'Zone auswählen',
     ['dialog_title'] = 'Sperrzone errichten',
     ['dialog_msg'] = 'Nachricht',
-    ['dialog_msg_desc'] = 'Was wird im Dispatch angezeigt?',
+    ['dialog_msg_desc'] = 'Was wird im Dispatch angezeigt - z.B. Straßenname',
     ['dialog_radius'] = 'Radius (Meter)',
     ['dialog_reason'] = 'Grund',
-    ['dialog_reason_desc'] = 'z.B. Schwerer Verkehrsunfall',
+    ['dialog_reason_desc'] = 'z. B. Schwerer Verkehrsunfall',
     ['dialog_speed'] = 'NPC Geschwindigkeit',
-    ['dialog_speed_desc'] = '0 = Stop, 30 = Normal',
+    ['dialog_speed_desc'] = '0 = Stopp, 30 = Normal',
     ['dialog_duration'] = 'Dauer (Minuten)',
-    ['dispatch_title'] = 'Los Santos Police Department',
     ['dispatch_subject'] = 'SPERRZONE ERRICHTET',
     ['dispatch_reason_prefix'] = '\nGrund: ~y~%s~s~',
     ['dispatch_update'] = 'UPDATE',
-    ['dispatch_cleared'] = 'Alle aktiven Sperrzonen wurden soeben aufgehoben.',
-    ['keybind_desc'] = 'Sperrzonen Menü öffnen'
+    ['dispatch_cleared2'] = 'Eine aktive Sperrzone wurde soeben aufgehoben.',
+    ['keybind_desc'] = 'Sperrzonen Menü öffnen',
+    ['no_active_zones'] = 'Es gibt aktuell keine aktiven Sperrzonen.'
 }
