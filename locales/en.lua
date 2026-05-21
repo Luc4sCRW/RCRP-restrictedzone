ba3db6c74e3b9b3ad8f2e4ab69b1b52333b6d880
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-05-21 16:05:55 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,23 +1,24 @@
 Locales['en'] = {
     ['access_denied'] = 'Access denied',
-    ['menu_title'] = 'Restricted Zone Management',
+    ['menu_title'] = 'Restricted Zone Management', -- Fallback
     ['create_zone'] = 'Create new restricted zone',
     ['create_zone_desc'] = 'Create zone at current position',
-    ['delete_zones'] = 'Delete all restricted zones',
-    ['delete_zones_desc'] = 'Remove all active zoones',
+    ['delete_spec_zones'] = 'Delete restricted zone',
+    ['delete_spec_zones_desc'] = 'Displays a list of all active zones',
+    ['delete_select'] = 'Select a zone',
     ['dialog_title'] = 'Set up restricted zone',
     ['dialog_msg'] = 'Message',
-    ['dialog_msg_desc'] = 'What is displayed in the dispatch?',
+    ['dialog_msg_desc'] = 'What is displayed in the dispatch - e.g. street name',
     ['dialog_radius'] = 'Radius (meters)',
     ['dialog_reason'] = 'Reason',
     ['dialog_reason_desc'] = 'e.g. Serious traffic accident',
     ['dialog_speed'] = 'NPC speed',
     ['dialog_speed_desc'] = '0 = Stop, 30 = Normal',
     ['dialog_duration'] = 'Duration (minutes)',
-    ['dispatch_title'] = 'Los Santos Police Department',
     ['dispatch_subject'] = 'RESTRICTED AREA ESTABLISHED',
     ['dispatch_reason_prefix'] = '\nReason: ~y~%s~s~',
     ['dispatch_update'] = 'UPDATE',
-    ['dispatch_cleared'] = 'All active restricted zones have just been lifted.',
-    ['keybind_desc'] = 'Open restricted zones menu'
+    ['dispatch_cleared2'] = 'A restricted zone has just been lifted.',
+    ['keybind_desc'] = 'Open the Restricted Zone Menu',
+    ['no_active_zones'] = 'There are currently no active restricted zones.'
 }
