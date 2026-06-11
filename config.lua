61fbfba0e2fe4fa668ce98d85a2732a50a111b41
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-06-11 19:05:37 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,32 +1,32 @@
 Config = {}
 
-Config.Locale = 'en' -- 'de', 'en' (add your own language if you want)
+Config.Locale = "en" -- "de", "en" (add your own language if you want)
 
-Config.NotifyType = 'gta' -- 'bulletin', 'esx', 'gta', 'ox', 'custom' (next update will fix ox message and esx message) | Recommendation: BULLETIN, GTA! | add your own export in the server.lua
+Config.NotifyType = "gta" -- "bulletin", "esx", "gta", "ox", "custom" (next update will fix ox message and esx message) | Recommendation: BULLETIN, GTA! | add your own export in the server.lua
 
 Config.AuthorizedJobs = {
-    ['police'] = {
+    ["police"] = {
         label = "Los Santos Police Department",
         blipColor = 3, -- Blue
         menuTitle = "LSPD Sperrzonen",
         icon = "CHAR_CALL911",
         deleteOther = false -- If set to “false” that job is only allowed to remove its own restricted zones
     },
-    ['noose'] = {
+    ["noose"] = {
         label = "NOOSE Tactical Unit",
         blipColor = 5, -- Yellow
         menuTitle = "NOOSE Sperrzonen",
         icon = "CHAR_CALL911",
         deleteOther = true
     },
-    ['ambulance'] = {
+    ["ambulance"] = {
         label = "San Andreas Medical Services",
         blipColor = 6, -- Light Red
         menuTitle = "SAMS Sperrzonen",
         icon = "CHAR_CALL911",
         deleteOther = false
     },
-    ['lsfd'] = {
+    ["lsfd"] = {
         label = "Los Santos Fire Department",
         blipColor = 1, -- Red
         menuTitle = "LSFD Sperrzonen",
@@ -36,21 +36,21 @@ Config.AuthorizedJobs = {
 }
 
 Config.RadiusOptions = {
-    { label = '10m', value = 10.0 },
-    { label = '20m', value = 20.0 },
-    { label = '30m', value = 30.0 },
-    { label = '50m', value = 50.0 },
-    { label = '80m', value = 80.0 },
-    { label = '100m', value = 100.0 },
-    { label = '150m', value = 150.0 },
-    { label = '200m', value = 200.0 },
-    { label = '300m', value = 300.0 },
+    { label = "10m", value = 10.0 },
+    { label = "20m", value = 20.0 },
+    { label = "30m", value = 30.0 },
+    { label = "50m", value = 50.0 },
+    { label = "80m", value = 80.0 },
+    { label = "100m", value = 100.0 },
+    { label = "150m", value = 150.0 },
+    { label = "200m", value = 200.0 },
+    { label = "300m", value = 300.0 },
 }
 
 Config.TimeOptions = {
-    { label = '5 Min', value = 5 },
-    { label = '10 Min', value = 10 },
-    { label = '20 Min', value = 20 },
-    { label = '30 Min', value = 30 },
-    { label = '60 Min', value = 60 }
+    { label = "5 Min", value = 5 },
+    { label = "10 Min", value = 10 },
+    { label = "20 Min", value = 20 },
+    { label = "30 Min", value = 30 },
+    { label = "60 Min", value = 60 }
 }
